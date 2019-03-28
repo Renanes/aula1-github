3dbc27a682d8040fb06bb5b98027bf19d583ceae
Renan Gomes <renan.gomes2323@hotmail.com>
2019-03-28 09:26:59 -0300

    Acrescentada a mensagem de Olá!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package application;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
 public class Program {
+    public static void main(String[] args){
+        System.out.println("Olá!");
+    }
 
 }
